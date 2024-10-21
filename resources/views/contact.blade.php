79e990ad97e6a74f516298130d79a4e7bfc63214
Imanuel Rian Rantelambung <imanuelrian22@gmail.com>
2024-10-21 20:05:16 +0900

    belajar route

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman About</title>
+    <title>Contact</title>
 </head>
 <body>
-<h1>About  {{$nama ?? ""}}</h1>
+<h1>contact</h1>
 <p>
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis eaque libero qui doloribus quidem! Explicabo adipisci nam dolorem suscipit amet ut aut, deleniti maiores ea recusandae possimus iste omnis nemo enim perspiciatis maxime ipsum voluptates tempore est reiciendis sed quasi? Odit itaque maiores id voluptatum tempore, deserunt quibusdam fugit magnam!
 </p>
